a59bf993a936b6b4efda8f18c72748d5d1ab7230
sunlike0508 <thesin1989@gmail.com>
2020-04-24 21:58:25 +0900

    2020-04-24-스프링부트-logback설정하기

--- src/main/java/board/board/controller/BoardController.java
+++ src/main/java/board/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package board.board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 import board.board.dto.BoardDto;
 import board.board.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class BoardController {
+	
+	//private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private BoardService boardService; 
 	
 	@RequestMapping("/board/openBoardList.do")
 	public ModelAndView openBoardList() throws Exception{
+		
+		log.debug("oepnBoardList");
+		
 		ModelAndView mv = new ModelAndView("/board/boardList");
 		
 		List<BoardDto> list = boardService.selectBoardList();
